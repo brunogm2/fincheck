82dafb750d228c6571a9c981561997b574538ea0
Bruno Gonçalves <63273269+brunogm2@users.noreply.github.com>
2023-09-22 01:25:37 -0300

    init front

---
+++ frontend/src/view/layouts/AuthLayout.tsx
@@ -0,0 +1,27 @@
+import ilustration from '../../assets/ilustration.png';
+
+import { Outlet } from 'react-router-dom';
+
+export function AuthLayout() {
+    return(
+        <div className="flex w-full h-full">
+            <div className="w-1/2 h-full"></div>
+
+            <div className="w-1/2 h-ful flex justify-center items-center p-8 relative">
+                <img
+                    src={ilustration}
+                    className="object-cover w-full h-full max-w-[656px] max-h-[960px] select-none
+                    rounded-[32px]"
+                    alt=""
+                />
+
+                <div className="max-w-[656px] bg-white bottom-8 p-10 absolute rounded-b-[32px]">
+                    <p>
+                        Gerencie suas finanças pessoais de uma forma simples
+                        com o fincheck, e o melhor, totalmente de graça!
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
