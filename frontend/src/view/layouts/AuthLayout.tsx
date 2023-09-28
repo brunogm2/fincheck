7fafd36c774ab6c69329e7365769dd20e4f83549
Bruno Gonçalves <63273269+brunogm2@users.noreply.github.com>
2023-09-27 22:12:10 -0300

    feat: login/register ui

--- frontend/src/view/layouts/AuthLayout.tsx
+++ frontend/src/view/layouts/AuthLayout.tsx
@@ -1,11 +1,19 @@
+import { Outlet } from 'react-router-dom';
 import ilustration from '../../assets/ilustration.png';
+import { Logo } from '../components/Logo';
 
 export function AuthLayout() {
     return(
         <div className="flex w-full h-full">
-            <div className="w-1/2 h-full"></div>
+            <div className="w-full h-full flex items-center justify-center flex-col gap-16 lg:w-1/2">
+                <Logo className="h-6 text-gray-500"/>
 
-            <div className="w-1/2 h-ful flex justify-center items-center p-8 relative">
+                <div className="w-full max-w-[504px] px-8">
+                    <Outlet />
+                </div>
+            </div>
+
+            <div className="w-1/2 h-ful justify-center items-center p-8 relative hidden lg:flex">
                 <img
                     src={ilustration}
                     className="object-cover w-full h-full max-w-[656px] max-h-[960px] select-none
@@ -13,8 +21,10 @@ export function AuthLayout() {
                     alt=""
                 />
 
-                <div className="max-w-[656px] bg-white bottom-8 p-10 absolute rounded-b-[32px]">
-                    <p className="text-gray-700 font-medium text-">
+                <div className="max-w-[656px] bg-white bottom-8 mx-8 p-10 absolute rounded-b-[32px]">
+                    <Logo className="text-teal-900 h-8" />
+
+                    <p className="text-gray-700 font-medium text-xl mt-6">
                         Gerencie suas finanças pessoais de uma forma simples
                         com o fincheck, e o melhor, totalmente de graça!
                     </p>
